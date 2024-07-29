d6b71de8acbbfdab0023f34c9d8133421bbb5c2b
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-07-30 08:44:26 +1000

    added figlet support

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 
 use args::cli;
 use jiff::{civil::DateTime, ToSpan, Zoned};
+use figlet_rs::FIGfont;
 
 mod args;
 
@@ -12,17 +13,31 @@ fn main() {
 
   thread::scope(|s| {
     s.spawn(|| {
+      let standard_font = FIGfont::standard().unwrap();
+      // TODO: Have a way to specify a font file
+      // let standard_font = FIGfont::from_file("bulbhead.flf").unwrap();
       let mut now: DateTime = Zoned::now().datetime();
       let now_plus_five = now.checked_add(5.seconds()).unwrap();
+      let diff = now_plus_five.since(now).unwrap();
+      let diff_string = format!("{}:{}:{}", diff.get_hours(), diff.get_minutes(), diff.get_seconds());
+      let fig_diff = standard_font.convert(&diff_string).unwrap();
+      let lines = fig_diff.height;
+      println!("{}", standard_font.convert(&diff_string).unwrap());
 
       while now <= now_plus_five {
+        for line in 0 .. lines+1 {
+          print!("\x1b[1A\x1b[0K");
+        }
+
         let diff = now_plus_five.since(now).unwrap();
-        print!("\r{}:{}:{}", diff.get_hours(), diff.get_minutes(), diff.get_seconds());
-        std::io::stdout().flush().unwrap();
+        let diff_string = format!("{}:{}:{}", diff.get_hours(), diff.get_minutes(), diff.get_seconds());
+        let fig_diff = standard_font.convert(&diff_string).unwrap();
+        println!("{}", standard_font.convert(&diff_string).unwrap());
         thread::sleep(Duration::from_millis(250));
         now = Zoned::now().datetime();
       }
     });
   })
 }
+
 
