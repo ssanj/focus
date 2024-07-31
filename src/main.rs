fb09d46969c3ef541edb1ec1e8f0878648c8b226
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-07-31 17:55:13 +1000

    add music and notifications

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use std::time::Duration;
 use args::cli;
 use jiff::{civil::DateTime, ToSpan, Zoned};
 use figlet_rs::FIGfont;
+use notify_rust::{Notification, Timeout};
+
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
+
 
 mod args;
 
@@ -17,7 +23,7 @@ fn main() {
       // TODO: Have a way to specify a font file
       // let standard_font = FIGfont::from_file("bulbhead.flf").unwrap();
       let mut now: DateTime = Zoned::now().datetime();
-      let now_plus_five = now.checked_add(5.seconds()).unwrap();
+      let now_plus_five = now.checked_add(25.seconds()).unwrap();
       let diff = now_plus_five.since(now).unwrap();
       let diff_string = format!("{}:{}:{}", diff.get_hours(), diff.get_minutes(), diff.get_seconds());
       let fig_diff = standard_font.convert(&diff_string).unwrap();
@@ -36,8 +42,40 @@ fn main() {
         thread::sleep(Duration::from_millis(250));
         now = Zoned::now().datetime();
       }
+
+      display_notification();
+      play_sound();
     });
   })
+}
+
+
+fn display_notification() {
+    Notification::new()
+        .summary("pom")
+        .subtitle("Time's Up!")
+        .timeout(Timeout::Milliseconds(3000)) //milliseconds
+        .show()
+        .unwrap();
+}
+
+
+fn play_sound() {
+
+// https://soundbible.com/1630-Computer-Magic.html
+
+  // Get an output stream handle to the default physical sound device
+  let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+  // Load a sound from a file, using a path relative to Cargo.toml
+  let file = BufReader::new(File::open("/Users/sanj/Downloads/Computer_Magic-Microsift-1901299923.mp3").unwrap());
+  // Decode that sound file into a source
+  let source = Decoder::new(file).unwrap();
+  // Play the sound directly on the device
+  let _ = stream_handle.play_raw(source.convert_samples());
+
+  // The sound plays in a separate audio thread,
+  // so we need to keep the main thread alive while it's playing.
+  std::thread::sleep(std::time::Duration::from_secs(5));
 }
 
 
