f6cab10da7fb8cdeb60e343d488e44fe7e888fc9
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-08-01 06:42:35 +1000

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::io::Write;
 use std::thread;
 use std::time::Duration;
 
@@ -16,51 +15,54 @@ mod args;
 
 fn main() {
   let args = cli::get_cli_args();
-
   let minutes = args.minutes as i32;
 
-  thread::scope(|s| {
-    s.spawn(|| {
-      let standard_font = FIGfont::standard().unwrap();
-      let mut now: DateTime = Zoned::now().datetime();
-      let now_plus_minutes_requested = now.checked_add(minutes.minutes()).unwrap();
-      let diff = now_plus_minutes_requested.since(now).unwrap();
-      let diff_string = format!("{}:{}:{}", diff.get_hours(), diff.get_minutes(), diff.get_seconds());
-      let fig_diff = standard_font.convert(&diff_string).unwrap();
-      let lines = fig_diff.height;
-      println!("{}", standard_font.convert(&diff_string).unwrap());
-
-      // Clear used lines from bottom to top
-      // See: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
-      while now <= now_plus_minutes_requested {
-        for _ in 0 .. lines+1 {
-          // \x1b[1A - Move up a line
-          // \x1b[0K - Erase from cursor until end of line
-          print!("\x1b[1A\x1b[0K");
-        }
-
-        let diff = now_plus_minutes_requested.since(now).unwrap();
-        let diff_string = format!("{}:{}:{}", diff.get_hours(), diff.get_minutes(), diff.get_seconds());
-        println!("{}", standard_font.convert(&diff_string).unwrap());
-        thread::sleep(Duration::from_millis(250));
-        now = Zoned::now().datetime();
-      }
-
-    });
-  });
-
+  display_timer(minutes);
   display_notification();
-  play_sound();
+  play_sound(); // This blocks the thread, run it last
+}
+
+fn display_timer(minutes: i32) {
+  thread::scope(|s| {
+      s.spawn(|| {
+        let mut now: DateTime = Zoned::now().datetime();
+        let now_plus_minutes_requested = now.checked_add(minutes.minutes()).unwrap();
+        let diff = now_plus_minutes_requested.since(now).unwrap();
+
+        let standard_font = FIGfont::standard().unwrap();
+        let diff_string = format!("{}:{}:{}", diff.get_hours(), diff.get_minutes(), diff.get_seconds());
+        let fig_diff = standard_font.convert(&diff_string).unwrap();
+        let lines = fig_diff.height;
+        println!("{}", standard_font.convert(&diff_string).unwrap());
+
+        // Clear used lines from bottom to top
+        // We go from bottom to top, so the next line is ready to be printed
+        // See: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+        while now <= now_plus_minutes_requested {
+          for _ in 0 .. lines+1 {
+            // \x1b[1A - Move up a line
+            // \x1b[0K - Erase from cursor until end of line
+            print!("\x1b[1A\x1b[0K");
+          }
+
+          let diff = now_plus_minutes_requested.since(now).unwrap();
+          let diff_string = format!("{}:{}:{}", diff.get_hours(), diff.get_minutes(), diff.get_seconds());
+          println!("{}", standard_font.convert(&diff_string).unwrap());
+          thread::sleep(Duration::from_millis(250));
+          now = Zoned::now().datetime();
+        }
+      });
+    });
 }
 
 
 fn display_notification() {
     Notification::new()
-        .summary("Focus")
-        .subtitle("Time's Up!")
-        .timeout(Timeout::Milliseconds(3000)) //milliseconds
-        .show()
-        .unwrap();
+      .summary("Focus")
+      .subtitle("Time's Up!")
+      .timeout(Timeout::Milliseconds(3000)) //milliseconds
+      .show()
+      .unwrap();
 }
 
 
