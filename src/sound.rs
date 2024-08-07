cb70c2c549763566947b0dc216c2edeb92b56d01
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-08-08 07:40:13 +1000

    fix issue with default sound file path

--- src/sound.rs
+++ src/sound.rs
@@ -1,13 +1,14 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{Cursor, Read, Write};
 use rodio::{source::Source, Decoder, OutputStream, Sink};
 
 use crate::error::{FocusAction, FocusError};
 
-pub fn play_sound(sound_file: Option<PathBuf>) -> FocusAction {
+const DEFAULT_SOUND: &'static [u8; 81970] = include_bytes!("assets/sounds/default.mp3");
 
-  // See: https://soundbible.com for free sounds
+// See: https://soundbible.com for free sounds
+pub fn play_sound(sound_file: Option<PathBuf>) -> FocusAction {
   let (_stream, stream_handle) =
     OutputStream::try_default()
       .map_err(|e| FocusError::CouldNotOpenSoundDevice(e.to_string()))?;
@@ -16,15 +17,29 @@ pub fn play_sound(sound_file: Option<PathBuf>) -> FocusAction {
     Sink::try_new(&stream_handle)
       .map_err(|e| FocusError::CouldNotCreateSinkForAudioPlayback(e.to_string()))?;
 
-  let sound_to_play = sound_file.unwrap_or_else(|| Path::new("src/assets/sounds/default.mp3").to_owned());
+  let mut sound_buffer = vec![];
 
-  let sound =
-    File::open(&sound_to_play)
-      .map_err(|e| FocusError::CouldNotOpenSoundFile(sound_to_play.to_string_lossy().to_string(), e.to_string()))?;
+  let _ = sound_file
+    .map(|file_name| {
+      File::open(&file_name)
+        .map_err(|e| FocusError::CouldNotOpenSoundFile(file_name.to_string_lossy().to_string(), e.to_string()))
+        .map(|mut file| {
+          let _ = file.read_to_end(&mut sound_buffer).expect("could not read sound file into buffer");
+          ()
+        })
+    })
+    .unwrap_or_else(|| {
+      Ok(
+          sound_buffer
+            .write_all(DEFAULT_SOUND)
+            .map(|_| ()).unwrap()
+      )
+    })?;
 
-  let file = BufReader::new(sound);
+
+  let cursor = Cursor::new(sound_buffer);
   let source =
-    Decoder::new(file)
+    Decoder::new(cursor)
       .map_err(|e| FocusError::CouldNotCreateDecoder(e.to_string()))?;
   sink.append(source.convert_samples::<f32>());
   sink.sleep_until_end();
